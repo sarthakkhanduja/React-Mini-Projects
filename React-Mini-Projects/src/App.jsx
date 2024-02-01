e5b3378e99e4717007401c0e23957333bfde0962
Sarthak Khanduja <sarthakkhanduja98@gmail.com>
2024-02-01 17:02:08 -0500

    working prototype without component distinction

--- React-Mini-Projects/src/App.jsx
+++ React-Mini-Projects/src/App.jsx
@@ -10,10 +10,50 @@ function App() {
     setBgColor(color);
   }, [bgColor]);
 
+  const buttons = [{
+      label: "Red",
+      backgroundColor: "Red",
+      color: "White"
+  }, {
+      label: "Green",
+      backgroundColor: "Green",
+      color: "White"
+  }, {
+      label: "Yellow",
+      backgroundColor: "Yellow",
+      color: "Black"
+  }, {
+      label: "Black",
+      backgroundColor: "Black",
+      color: "White"
+  }, {
+      label: "Purple",
+      backgroundColor: "Purple",
+      color: "White"
+  }, {
+      label: "Blue",
+      backgroundColor: "Blue",
+      color: "white"
+  }, {
+      label: "Default",
+      backgroundColor: "Beige",
+      color: "Black"
+  }]
+
   return (
     <div className="main-div" style={{backgroundColor: bgColor}}>
-      <BackgroundChanger onColorChange = {handleColorChange}/>
+      {buttons.map((button) => {
+        return(<Button label={button.label} backgroundColor={button.backgroundColor} color={button.color} onColorChange={() => {
+          handleColorChange(button.backgroundColor);
+        }}/>)                
+      })}
     </div>
+  )
+}
+
+function Button({label, backgroundColor, color, onColorChange}) {
+  return(
+      <button className="btn" style={{backgroundColor: backgroundColor, color: color}} onClick={onColorChange}>{label}</button>
   )
 }
 
